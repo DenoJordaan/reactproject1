ae7415fe72fd55ce6ea49246f26057c770c51219
Deno Jordaan <djordaancheat@gmail.com>
2020-04-28 12:54:54 +0200

    chapter 2 finished

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Products from './Products';
 class App extends Component {
   render() {
     return (
         <div>
           <h1>
-            My First React App!
+              My First React App!
+              <Products/>
           </h1>
         </div>
     );
