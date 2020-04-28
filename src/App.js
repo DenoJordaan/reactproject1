310488d1906bb12c4a34bb7bccc1d641c5ee1da5
Deno Jordaan <djordaancheat@gmail.com>
2020-04-28 13:21:36 +0200

    chapter 3 finished

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
 import Products from './Products';
+import { Button } from 'react-bootstrap';
+import Rating from './Rating';
+
 class App extends Component {
   render() {
-    return (
+      const isValid = true;
+
+      return (
         <div>
           <h1>
               My First React App!
               <Products/>
+              <Rating rating="1"/>
+              <Rating rating="2"/>
+              <Rating rating="3"/>
+              <Rating rating="4"/>
+              <Rating rating="5"/>
+              <Button varient="primary" disabled={!isValid}>Default</Button>
           </h1>
         </div>
     );
