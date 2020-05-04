6f511d362aa82edd7cd00b3ffa7e68e80f5be8b6
Deno Jordaan <djordaancheat@gmail.com>
2020-05-04 10:41:00 +0200

    chapter 6 finished

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import React, { Component } from 'react';
-import { Jumbotron, Button } from 'react-bootstrap';
+import UserForm from './UserForm';
 
-class JumboTronComponent extends Component {
-    constructor(props){
-        super(props);
-    }render() {
+class App extends Component {
+    render() {
         return (
             <div>
-                <Jumbotron>
-                    <h1>Hello, world!</h1>
-                    <p>{this.props.children}</p>
-                    <p><Button variant="primary">Learn more</Button></p>
-                </Jumbotron>
+                <UserForm />
+
             </div>
         );
     }
 }
-export default JumboTronComponent;
+
+export default App;
+
+//import Products from './Products';
