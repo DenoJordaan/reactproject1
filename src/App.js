bb9a77d5c7886280c2c52a859aabf2d135b6951c
Deno Jordaan <djordaancheat@gmail.com>
2020-05-04 11:08:44 +0200

    chapter 7 finished

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import React, { Component } from 'react';
-import UserForm from './UserForm';
+import GitHub from './GitHub';
 
 class App extends Component {
+
     render() {
         return (
             <div>
-                <UserForm />
-
+                <GitHub />
             </div>
         );
     }
 }
 
 export default App;
-
-//import Products from './Products';
